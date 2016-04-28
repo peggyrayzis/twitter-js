80888971f50a35a09e77a9f5b9d84134590e918d
Peggy Rayzis <peggyrayzis@gmail.com>
2016-04-28 12:49:15 -0400

    Got up to Forms & HTTP Req - Dynamically Pre-Populating Fields

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,47 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
+
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({
+extended: true
+}));
+router.use(bodyParser.json());
+
 router.get(express.static(__dirname+'/public'));
 
 router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+	var tweets = tweetBank.list();
+	res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true});
+
+});
+
+router.post('/tweets', function(req, res) {
+	console.log("body:",req)
+	var name = req.body.name;
+	var text = req.body.text;
+	tweetBank.add(name, text);
+	res.redirect('/');
+});
+
+// router.get('/tweets', function(req, res) {
+//   	var tweets = tweetBank.list();
+//   	res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true});
+// });
+
+router.get('/users/:name', function(req, res) {
+	var name = req.params.name;
+	var list = tweetBank.find({name:name});
+	res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list } );
+});
+
+router.get('/tweets/:id', function(req, res) {
+	var id = req.params.id;
+	var list = tweetBank.find({id:id});
+	res.render( 'index', { title: 'Twitter.js - Post ID:'+id, tweets: list } );
 });
 
 module.exports = router;
--- tweetBank.js
+++ tweetBank.js
@@ -1,11 +1,12 @@
 var _ = require('lodash');
 
+
 // Data storage
 var data = [];
 
 // Module functions
-function add (name, text) {
-  data.push({ name: name, text: text });
+function add (name, text, id) {
+  data.push({ name: name, text: text, id: id });
 }
 
 function list () {
@@ -19,3 +20,7 @@ function find (properties) {
 // Export the module functions
 module.exports = { add: add, list: list, find: find };
 
+// Add some data and export it...
+module.exports.add('SondreLerche', "That's a Volkswagen!", "1");
+module.exports.add('RayRomano', "I feel like this is a dream...", "2");
+module.exports.add('TheCynic', "Aim for the moon...", "3");
