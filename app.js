a65c07afda32390c8a711eb1564098b815f41f1b
Peggy Rayzis <peggyrayzis@gmail.com>
2016-04-28 10:39:34 -0400

    Working twitter site.

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
 var express = require( 'express' );
-var http = require('http')
 var app = express();
-var morgan = require('morgan')
+
 var swig = require('swig')
 
+var http = require('http')
 var server = http.createServer();
 var port = 3000;
+server.on('request', app);
 
-server.on('request', app)
+// Routes 
+var routes = require('./routes/');
+app.use('/',routes);
+
+var morgan = require('morgan')
 
 app.use(morgan(':method :url :status'))
 
-var people = [{name: '@peggy'}, {name: '@patrice'}, {name: '@gracehopper'}]
-var hashtags = [{topic: '#express'}, {topic: '#node'}, {topic: '#lacolombe'}];
-
-app.get('/', function(req, res){
-	res.render( 'index', {title: 'Twitter.js', people: people, hashtags: hashtags} );
-	// res.send("app works")
-})
-
-app.get('/news', function(req, res){
-	res.send("news works")
-})
-
-app.get('/special/', function(req, res){
-	res.send("were special")
-})
 
 app.listen(3000, function(){
-	console.log("our server works")
+	console.log("Working Server: Port 3000")
 })
 
 app.engine('html', swig.renderFile);
@@ -36,6 +26,6 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 swig.setDefaults({ cache: false })
-swig.renderFile(__dirname + '/views/index.html', people, function (err, output) {
+swig.renderFile(__dirname + '/views/index.html', function (err, output) {
     console.log(output);
 });
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+// could use one line instead: var router = require('express').Router();
+var tweetBank = require('../tweetBank');
+
+router.get(express.static(__dirname+'/public'));
+
+router.get('/', function (req, res) {
+  var tweets = tweetBank.list();
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+});
+
+module.exports = router;
